fcc758c032d38555a02dacb978f4828732507fe8
Abhishek Kumar <abhishekk580@gmail.com>
2024-03-10 00:19:11 +0530

    isPriceInRange parametrized

--- tests/core.test.js
+++ tests/core.test.js
@@ -209,3 +209,15 @@ describe("canDrive parametrized", () => {
     expect(canDrive(age, country)).toBe(result);
   });
 });
+
+describe("isPriceInRange parametrized", () => {
+  it.each([
+    { scenario: "price < min", price: -10, result: false },
+    { scenario: "price = min", price: 0, result: true },
+    { scenario: "price between min and max", price: 50, result: true },
+    { scenario: "price = max", price: 100, result: true },
+    { scenario: "price > max", price: 200, result: false },
+  ])("should return $result when $scenario", ({ price, result }) => {
+    expect(isPriceInRange(price, 0, 100)).toBe(result);
+  });
+});
