a75835920e1e1267660b722f7670443be7059496
Abhishek Kumar <abhishekk580@gmail.com>
2024-03-11 00:01:29 +0530

    isOnline - test time

--- tests/mocking.test.js
+++ tests/mocking.test.js
@@ -2,6 +2,7 @@ import { beforeEach, describe, expect, it, vi } from "vitest";
 import {
   getPriceInCurrency,
   getShippingInfo,
+  isOnline,
   login,
   renderPage,
   signUp,
@@ -184,5 +185,23 @@ describe("login", () => {
 
     const code = spy.mock.results[0].value.toString();
     expect(sendEmail).toHaveBeenCalledWith(email, code);
+  });
+});
+
+describe("isOnline", () => {
+  it("should return false if current hour is outside opening hours", () => {
+    vi.setSystemTime("2024-01-01 07:59");
+    expect(isOnline()).toBe(false);
+
+    vi.setSystemTime("2024-01-01 20:01");
+    expect(isOnline()).toBe(false);
+  });
+
+  it("should return true if current hour is within opening hours", () => {
+    vi.setSystemTime("2024-01-01 08:00");
+    expect(isOnline()).toBe(true);
+
+    vi.setSystemTime("2024-01-01 19:59");
+    expect(isOnline()).toBe(true);
   });
 });
