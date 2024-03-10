5b95ccb701d55685b71b51d3dcf3d85c8b302a40
Abhishek Kumar <abhishekk580@gmail.com>
2024-03-10 23:47:38 +0530

    spy

--- tests/mocking.test.js
+++ tests/mocking.test.js
@@ -2,6 +2,7 @@ import { describe, expect, it, vi } from "vitest";
 import {
   getPriceInCurrency,
   getShippingInfo,
+  login,
   renderPage,
   signUp,
   submitOrder,
@@ -11,6 +12,7 @@ import { getShippingQuote } from "../src/libs/shipping";
 import { trackPageView } from "../src/libs/analytics";
 import { charge } from "../src/libs/payment";
 import { sendEmail } from "../src/libs/email";
+import security from "../src/libs/security";
 
 vi.mock("../src/libs/currency");
 vi.mock("../src/libs/shipping");
@@ -162,5 +164,17 @@ describe("signUp", () => {
     const args = vi.mocked(sendEmail).mock.calls[0];
     expect(args[0]).toBe(email);
     expect(args[1]).toMatch(/welcome/i);
+  });
+});
+
+describe("login", () => {
+  const email = "a@a.com";
+  it("should email the one-time login code", async () => {
+    const spy = vi.spyOn(security, "generateCode");
+
+    await login(email);
+
+    const code = spy.mock.results[0].value.toString();
+    expect(sendEmail).toHaveBeenCalledWith(email, code);
   });
 });
