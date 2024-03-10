4c525d6ad81ee03f9095f1b019ac0ee5c25ff5ad
Abhishek Kumar <abhishekk580@gmail.com>
2024-03-10 17:11:53 +0530

    getShippingInfo

--- tests/mocking.test.js
+++ tests/mocking.test.js
@@ -1,8 +1,10 @@
 import { describe, expect, it, vi } from "vitest";
-import { getPriceInCurrency } from "../src/mocking";
+import { getPriceInCurrency, getShippingInfo } from "../src/mocking";
 import { getExchangeRate } from "../src/libs/currency";
+import { getShippingQuote } from "../src/libs/shipping";
 
 vi.mock("../src/libs/currency");
+vi.mock("../src/libs/shipping");
 
 describe("test suite - mocking", () => {
   it("test case", () => {
@@ -58,5 +60,20 @@ describe("getPriceInCurrency", () => {
     const price = getPriceInCurrency(10, "AUD");
 
     expect(price).toBe(15);
+  });
+});
+
+describe("getShippingInfo", () => {
+  it("should return shipping unavailable if quote cannot be fetched", () => {
+    vi.mocked(getShippingQuote).mockReturnValue(null);
+    const result = getShippingInfo("London");
+    expect(result).toMatch(/unavailable/i);
+  });
+
+  it("should return shipping info if quote can be fetched", () => {
+    vi.mocked(getShippingQuote).mockReturnValue({ cost: 10, estimatedDays: 2 });
+    const result = getShippingInfo("London");
+    expect(result).toMatch("$10");
+    expect(result).toMatch(/2 days/i);
   });
 });
